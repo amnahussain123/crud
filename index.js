e74169c596e5dc6bce0acb09be64ed1748c22e5a
Amna Hussain <aman.hussain@invozone.com>
2021-12-30 18:06:18 +0500

    Sequilize CRUD(create, retrieve)

--- index.js
+++ index.js
@@ -1,17 +1,45 @@
-const db = require('./config');
-const express = require('express');
-const bodyParser = require('body-parser'); 
-const { get } = require('express/lib/response');
+// const db = require('./config');
+// const express = require('express');
+// const bodyParser = require('body-parser'); 
+// const { get } = require('express/lib/response');
+// const app = express();
+// app.set('view engine', 'ejs');
+// app.use(bodyParser.urlencoded({ extended: true }))
+// app.get('/',db.getUser);
+// app.post('/users', db.createUser);
+// app.get('/users', db.getUser);
+// // app.get('/users/:id', db.getUserById);
+// app.get('/users/edit/:id', db.getUserById);
+// app.get('/users/delete/:id', db.deleteUserById);
+// app.post('/users/update', db.updateUser);
+// app.listen(3000, function() {
+//     console.log('listening on 3000')
+//   })
+const express = require("express");
+const cors = require("cors");
+
 const app = express();
+const db = require("./app/models");
+db.sequelize.sync();
+var corsOptions = {
+  origin: "http://localhost:3000"
+};
+
+app.use(cors(corsOptions));
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({ extended: true }))
-app.get('/',db.getUser);
-app.post('/users', db.createUser);
-app.get('/users', db.getUser);
-// app.get('/users/:id', db.getUserById);
-app.get('/users/edit/:id', db.getUserById);
-app.get('/users/delete/:id', db.deleteUserById);
-app.post('/users/update', db.updateUser);
-app.listen(3000, function() {
-    console.log('listening on 3000')
-  })
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+// simple route
+// app.get("/", (req, res) => {
+//   res.render('index');
+// });
+require("./app/routes/testing.routes")(app);
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
